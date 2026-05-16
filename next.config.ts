999337659e35a2687d78d615929bdc56e2e293ac
Robson Correa <rcorreas@Mac-mini-de-Robson.local>
2026-05-16 17:51:11 -0300

    fix(next.config): add ytimg.com domains for next/image to prevent page crash

--- next.config.ts
+++ next.config.ts
@@ -64,6 +64,24 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i9.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
